d4d1cb038e49dc785b1b0fd60dc373c986d01705
Prince Eze <roi.prince@hotmail.fr>
2024-07-01 17:35:46 +0200

    second commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Home Page</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button>Start</button>
     </main>
   );
 }
